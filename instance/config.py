850416f49d1c0bcc718f9327df791f157fcea5bd
Arturo Ávila Martínez <arturoavilamartinez9@gmail.com>
2025-02-01 23:43:17 -0600

    Changes on database URI

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:@localhost:3306/task_manager_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://root:@localhost:3306/task_manager_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
     WTF_CSRF_ENABLED = True
